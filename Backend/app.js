e1487aeefdbceff9323727116a8ddb13b4b56519
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-02-14 20:47:47 +0530

    m

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 import userRouter from './routes/userRouter.js';
@@ -12,12 +11,6 @@ import { errorMiddleware } from './middlewares/error.js';
 //middlewares
 const app = express();
 dotenv.config({path : './config/config.env'});
-app.use(cors())
-//Connect backend with frontend
-app.use(cors({
-    methods : ['GET', 'PUT', 'POST', 'DELETE'],
-    credentials : true,
-}))
 
 //cookie parser
 app.use(cookieParser());
