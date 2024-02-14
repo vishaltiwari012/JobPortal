7adb19a66b5b9ac2af6e362f5f50b8d3635c4ea6
Vishal Tiwari <tiwarivishal95067@gmail.com>
2024-02-14 18:17:14 +0530

    change

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ dotenv.config({path : './config/config.env'});
 
 //Connect backend with frontend
 app.use(cors({
-    origin : [process.env.FRONTEND],
+    origin : [process.env.FRONTEND_URL],
     methods : ['GET', 'PUT', 'POST', 'DELETE'],
     credentials : true,
 }))
